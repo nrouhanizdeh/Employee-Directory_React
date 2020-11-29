1f57d7cd0763b661c5221a8b4eacb28750634725
Negin Rouhanizadeh <negin.rouhanizadeh@gmail.com>
2020-11-29 16:10:34 -0600

    initial commit -added files and folders and initial code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Navbar } from "react-bootstrap";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Brand href="#" style={{ color: "white" }}>
+          Employee Directory
+        </Navbar.Brand>
+      </Navbar>
+
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/index.js
@@ -0,0 +1,74 @@
+import { Form, Button, FormControl, Container } from "react-bootstrap";
+import React from "react";
+import Api from "../../utils/Api";
+import Table from "../Table/index";
+
+class Main extends React.Component {
+  state = {
+    employee: [],
+    search: "",
+    filterEmployee: [],
+  };
+
+  componentDidMount() {
+    this.searchEmployee();
+  }
+
+  searchEmployee() {
+    Api.getUsers().then((results) => {
+      this.setState({ employee: results.data.results });
+      this.handleFilteredEmployee();
+    });
+  }
+
+  handleInputChange = (e) => {
+    let value = e.target.value;
+    const name = e.target.name;
+    // Update the input's state
+    this.setState({
+      [name]: value,
+    });
+    this.handleFilteredEmployee();
+  };
+
+  handleFilteredEmployee = () => {
+    let newFilterEmployee;
+
+    if (this.state.search === "") {
+      newFilterEmployee = this.state.employee;
+    } else {
+      newFilterEmployee = this.state.employee.filter((employee) => {
+        return (
+          employee.name.first.toLowerCase().includes(this.state.search) ||
+          employee.name.last.toLowerCase().includes(this.state.search)
+        );
+      });
+    }
+    this.setState({
+      filterUsers: newFilterEmployee,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Container className="justify-content-center">
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              value={this.state.search}
+              name="search"
+              onChange={this.handleInputChange}
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Table users={this.state.filterUsers} />
+        </Container>
+        ;
+      </div>
+    );
+  }
+}
+export default Main;
